dab42ea1fbc06df4648991d2cbc2343a78fff174
Claudia melissa Gómez beltrán <claudiamelissagomezbeltran@MacBook-Pro-de-Claudia.local>
2018-12-27 16:44:34 +0000

    add date picker

--- Week4ExerciseSolution/app/src/main/java/oak/shef/ac/uk/week6/pickerActivity.java
+++ Week4ExerciseSolution/app/src/main/java/oak/shef/ac/uk/week6/pickerActivity.java
@@ -1,22 +1,17 @@
 package oak.shef.ac.uk.week6;
 
-
 import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import android.view.View;
-import android.widget.Button;
-
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import java.util.Calendar;
 
+public class pickerActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String CERO = "0";
     private static final String SLASH = "/";
     public final Calendar c = Calendar.getInstance();
@@ -25,30 +20,16 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     final int year = c.get(Calendar.YEAR);
     EditText theDate;
     ImageButton getDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_view);
-
-        Button buttonSearch = (Button)findViewById(R.id.buttonSearch);
-        final EditText textTitle = (EditText) findViewById(R.id.editTextTitle);
-        final EditText textDesc = (EditText) findViewById(R.id.editTextDescription);
         theDate = (EditText) findViewById(R.id.editTextDate);
-        getDate = (ImageButton) findViewById(R.id.buttonDate);
+       getDate = (ImageButton) findViewById(R.id.buttonDate);
         getDate.setOnClickListener(this);
-        buttonSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String title= textTitle.getText().toString();
-                String desc= textDesc.getText().toString();
-                Intent intent = new Intent(getBaseContext(), ShowSearchActivity.class);
-                intent.putExtra("TITLE", title);
-                intent.putExtra("DESC", desc);
-                startActivity(intent);
-            }
-        });
-    }
 
+    }
 
     @Override
     public void onClick(View v) {
@@ -65,16 +46,20 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 final int actualMonth = month + 1;
                 String fDay= (dayOfMonth < 10) ? CERO + String.valueOf(dayOfMonth) : String.valueOf(dayOfMonth);
+                //Formateo el mes obtenido: antepone el 0 si son menores de 10
                 String fMonth = (actualMonth < 10) ? CERO + String.valueOf(actualMonth) : String.valueOf(actualMonth);
+                //Muestro la fecha con el formato deseado
                 theDate.setText(fDay + SLASH + fMonth + SLASH + year);
 
 
             }
+            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+            /**
+             *También puede cargar los valores que usted desee
+             */
         }, year, month, day);
         //Muestro el widget
         datePicker.show();
 
     }
-
-
 }
